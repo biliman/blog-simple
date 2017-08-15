609efb6b29a4c55265018c2b67050fe639489893
Fickry Bil Iman <fickry@Fickrys-Macbook-Pro.local>
2017-08-15 13:31:32 +0700

    required on app.js

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const monggose = require('mongoose');
+const cors = require('cors');
+const log = require('morgan');
+
+const index = require('./routes/index')
+
+app.use(cors());
+require('dotenv').config();
+
+mongoose.connect('mongodb://localhost/blog-simple');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/*+json'}));
+app.use(bodyParser.json({type: 'application/x-www-form-urlencoded'}));
+
+app.use(log('dev'))
+
+app.use('/', index)
+
+app.listen(3000, () => {
+  console.log('We are alive at port : 3000');
+})
