715359071046a964b531b8bd8667847aa7b81046
Fickry Bil Iman <fickry@Fickrys-Macbook-Pro.local>
2017-08-15 14:42:59 +0700

    route article

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const articleController = require('../controllers/articleController')
 
+// USER
 router.post('/signup', userController.signUp);
 router.post('/signin', userController.signIn);
 router.get('/alluser', userController.getAllUser);
+
+//ARTICLE
+router.get('/allarticles', articleController.getAllArticles);
+router.get('/articlebyauthor/:id', articleController.getArticleByAuthor);
+router.get('/detailarticle/:id', articleController.getArticleDetail);
+router.post('/createarticle', articleController.createArticle);
+router.put('/editarticle/:id', articleController.editArticle);
+router.delete('/deletearticle/:id', articleController.deleteArticle);
 
 module.exports = router
